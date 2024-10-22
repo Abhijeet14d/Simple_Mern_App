ce5073ff77daaa441e15bc3f9d881567e202c200
Abhijeet14d <abhijeetsinghwork.01@gmail.com>
2024-10-22 16:22:53 +0530

    complete version 1

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
 const PORT = process.env.PORT || 5000;
 
-
+const __dirname = path.resolve();
 
 
 
@@ -21,6 +22,14 @@ app.use(cors(
 app.use(express.json()); // allows us to accept json data in the body
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.use((err, req, res, next) => {
     console.error('Server Error:', err);
